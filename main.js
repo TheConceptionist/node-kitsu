7c916d76fdd68909b8c7b0a1da6e28c95160fcc6
Odin LeLacheur <olelacheur@gmail.com>
2017-03-29 16:26:18 -0700

    0.4
    Promises™

--- main.js
+++ main.js
@@ -1,199 +1,229 @@
 var request = require("request");
 
-exports.searchAnime = function(query, offset, callback) {
-    request({
-        method: 'GET',
-        url: 'https://kitsu.io/api/edge/anime?filter[text]=' + query + '&page%5Boffset%5D=' + (offset.toString() ? offset : '0'),
-        headers: {
-            'Content-Type': 'application/vnd.api+json',
-            'Accept': 'application/vnd.api+json'
-        }
-    }, function(error, response, body) {
-        if (error) {
-            callback(error)
-        }
-        var allofit = JSON.parse(body)
-        var results = allofit.data
-        callback(null, results)
+exports.searchAnime = function(query, offset) {
+    return new Promise(function(resolve, reject) {
+        request({
+            method: 'GET',
+            url: 'https://kitsu.io/api/edge/anime?filter[text]=' + query + '&page%5Boffset%5D=' + (offset.toString() ? offset : '0'),
+            headers: {
+                'Content-Type': 'application/vnd.api+json',
+                'Accept': 'application/vnd.api+json'
+            }
+        }, function(error, response, body) {
+            if (error) {
+                reject(Error(error))
+            } else {
+                var allofit = JSON.parse(body)
+                var results = allofit.data
+                resolve(results)
+            }
+        })
     })
 }
 
-exports.listAnime = function(offset, callback) {
-    if (offset === null || offset === undefined) {
-        offset = 0
-    }
-    request({
-        method: 'GET',
-        url: 'https://kitsu.io/api/edge/anime?page%5Blimit%5D=10&page%5Boffset%5D=' + (offset.toString() ? offset : '0'),
-        headers: {
-            'Content-Type': 'application/vnd.api+json',
-            'Accept': 'application/vnd.api+json'
+exports.listAnime = function(offset) {
+    return new Promise(function(resolve, reject) {
+        if (offset === null || offset === undefined) {
+            offset = 0
         }
-    }, function(error, response, body) {
-        if (error) {
-            callback(error)
-        }
-        var allofit = JSON.parse(body)
-        var results = allofit.data
-        callback(null, results)
+        request({
+            method: 'GET',
+            url: 'https://kitsu.io/api/edge/anime?page%5Blimit%5D=10&page%5Boffset%5D=' + (offset.toString() ? offset : '0'),
+            headers: {
+                'Content-Type': 'application/vnd.api+json',
+                'Accept': 'application/vnd.api+json'
+            }
+        }, function(error, response, body) {
+            if (error) {
+                reject(Error(error))
+            } else {
+                var allofit = JSON.parse(body)
+                var results = allofit.data
+                resolve(results)
+            }
+        })
     })
 }
 
-exports.searchManga = function(query, offset, callback) {
-    request({
-        method: 'GET',
-        url: 'https://kitsu.io/api/edge/manga?filter[text]=' + query + '&page%5Boffset%5D=' + (offset.toString() ? offset : '0'),
-        headers: {
-            'Content-Type': 'application/vnd.api+json',
-            'Accept': 'application/vnd.api+json'
-        }
-    }, function(error, response, body) {
-        if (error) {
-            callback(error)
-        }
-        var allofit = JSON.parse(body)
-        var results = allofit.data
-        callback(null, results)
+exports.searchManga = function(query, offset) {
+    return new Promise(function(resolve, reject) {
+        request({
+            method: 'GET',
+            url: 'https://kitsu.io/api/edge/manga?filter[text]=' + query + '&page%5Boffset%5D=' + (offset.toString() ? offset : '0'),
+            headers: {
+                'Content-Type': 'application/vnd.api+json',
+                'Accept': 'application/vnd.api+json'
+            }
+        }, function(error, response, body) {
+            if (error) {
+                reject(Error(error))
+            } else {
+                var allofit = JSON.parse(body)
+                var results = allofit.data
+                resolve(results)
+            }
+        })
     })
 }
 
-exports.listManga = function(offset, callback) {
-    if (offset === null || offset === undefined) {
-        offset = 0
-    }
-    request({
-        method: 'GET',
-        url: 'https://kitsu.io/api/edge/manga?page%5Blimit%5D=10&page%5Boffset%5D=' + (offset.toString() ? offset : '0'),
-        headers: {
-            'Content-Type': 'application/vnd.api+json',
-            'Accept': 'application/vnd.api+json'
+exports.listManga = function(offset) {
+    return new Promise(function(resolve, reject) {
+        if (offset === null || offset === undefined) {
+            offset = 0
         }
-    }, function(error, response, body) {
-        if (error) {
-            callback(error)
-        }
-        var allofit = JSON.parse(body)
-        var results = allofit.data
-        callback(null, results)
+        request({
+            method: 'GET',
+            url: 'https://kitsu.io/api/edge/manga?page%5Blimit%5D=10&page%5Boffset%5D=' + (offset.toString() ? offset : '0'),
+            headers: {
+                'Content-Type': 'application/vnd.api+json',
+                'Accept': 'application/vnd.api+json'
+            }
+        }, function(error, response, body) {
+            if (error) {
+                reject(Error(error))
+            } else {
+                var allofit = JSON.parse(body)
+                var results = allofit.data
+                resolve(results)
+            }
+        })
     })
 }
 
-exports.searchDrama = function(query, offset, callback) {
-    request({
-        method: 'GET',
-        url: 'https://kitsu.io/api/edge/drama?filter[text]=' + query + '&page%5Boffset%5D=' + (offset.toString() ? offset : '0'),
-        headers: {
-            'Content-Type': 'application/vnd.api+json',
-            'Accept': 'application/vnd.api+json'
-        }
-    }, function(error, response, body) {
-        if (error) {
-            callback(error)
-        }
-        var allofit = JSON.parse(body)
-        var results = allofit.data
-        callback(null, results)
+exports.searchDrama = function(query, offset) {
+    return new Promise(function(resolve, reject) {
+        request({
+            method: 'GET',
+            url: 'https://kitsu.io/api/edge/drama?filter[text]=' + query + '&page%5Boffset%5D=' + (offset.toString() ? offset : '0'),
+            headers: {
+                'Content-Type': 'application/vnd.api+json',
+                'Accept': 'application/vnd.api+json'
+            }
+        }, function(error, response, body) {
+            if (error) {
+                reject(Error(error))
+            } else {
+                var allofit = JSON.parse(body)
+                var results = allofit.data
+                resolve(results)
+            }
+        })
     })
 }
 
-exports.listDrama = function(offset, callback) {
-    if (offset === null || offset === undefined) {
-        offset = 0
-    }
-    request({
-        method: 'GET',
-        url: 'https://kitsu.io/api/edge/drama?page%5Blimit%5D=10&page%5Boffset%5D=' + (offset.toString() ? offset : '0'),
-        headers: {
-            'Content-Type': 'application/vnd.api+json',
-            'Accept': 'application/vnd.api+json'
+exports.listDrama = function(offset) {
+    return new Promise(function(resolve, reject) {
+        if (offset === null || offset === undefined) {
+            offset = 0
         }
-    }, function(error, response, body) {
-        if (error) {
-            callback(error)
-        }
-        var allofit = JSON.parse(body)
-        var results = allofit.data
-        callback(null, results)
+        request({
+            method: 'GET',
+            url: 'https://kitsu.io/api/edge/drama?page%5Blimit%5D=10&page%5Boffset%5D=' + (offset.toString() ? offset : '0'),
+            headers: {
+                'Content-Type': 'application/vnd.api+json',
+                'Accept': 'application/vnd.api+json'
+            }
+        }, function(error, response, body) {
+            if (error) {
+                reject(Error(error))
+            } else {
+                var allofit = JSON.parse(body)
+                var results = allofit.data
+                resolve(results)
+            }
+        })
     })
 }
 
-exports.listUsers = function(offset, callback) {
-    if (offset === null || offset === undefined) {
-        offset = 0
-    }
-    request({
-        method: 'GET',
-        url: 'https://kitsu.io/api/edge/users?page%5Blimit%5D=10&page%5Boffset%5D=' + (offset.toString() ? offset : '0'),
-        headers: {
-            'Content-Type': 'application/vnd.api+json',
-            'Accept': 'application/vnd.api+json'
+exports.listUsers = function(offset) {
+    return new Promise(function(resolve, reject) {
+        if (offset === null || offset === undefined) {
+            offset = 0
         }
-    }, function(error, response, body) {
-        if (error) {
-            callback(error)
-        }
-        var allofit = JSON.parse(body)
-        var results = allofit.data
-        callback(null, results)
+        request({
+            method: 'GET',
+            url: 'https://kitsu.io/api/edge/users?page%5Blimit%5D=10&page%5Boffset%5D=' + (offset.toString() ? offset : '0'),
+            headers: {
+                'Content-Type': 'application/vnd.api+json',
+                'Accept': 'application/vnd.api+json'
+            }
+        }, function(error, response, body) {
+            if (error) {
+                reject(Error(error))
+            } else {
+                var allofit = JSON.parse(body)
+                var results = allofit.data
+                resolve(results)
+            }
+        })
     })
 }
 
-exports.getUser = function(username, callback) {
-    request({
-        method: 'GET',
-        url: 'https://kitsu.io/api/edge/users?filter[name]=' + username,
-        headers: {
-            'Content-Type': 'application/vnd.api+json',
-            'Accept': 'application/vnd.api+json'
-        }
-    }, function(error, response, body) {
-        if (error) {
-            callback(error)
-        }
-        var allofit = JSON.parse(body)
-        var results = allofit.data
-        callback(null, results)
+exports.getUser = function(username) {
+    return new Promise(function(resolve, reject) {
+        request({
+            method: 'GET',
+            url: 'https://kitsu.io/api/edge/users?filter[name]=' + username,
+            headers: {
+                'Content-Type': 'application/vnd.api+json',
+                'Accept': 'application/vnd.api+json'
+            }
+        }, function(error, response, body) {
+            if (error) {
+                reject(Error(error))
+            } else {
+                var allofit = JSON.parse(body)
+                var results = allofit.data
+                resolve(results)
+            }
+        })
     })
 }
 
-exports.listGenres = function(offset, callback) {
-    if (offset === null || offset === undefined) {
-        offset = 0
-    }
-    request({
-        method: 'GET',
-        url: 'https://kitsu.io/api/edge/genres?page%5Blimit%5D=10&page%5Boffset%5D=' + (offset.toString() ? offset : '0'),
-        headers: {
-            'Content-Type': 'application/vnd.api+json',
-            'Accept': 'application/vnd.api+json'
+exports.listGenres = function(offset) {
+    return new Promise(function(resolve, reject) {
+        if (offset === null || offset === undefined) {
+            offset = 0
         }
-    }, function(error, response, body) {
-        if (error) {
-            callback(error)
-        }
-        var allofit = JSON.parse(body)
-        var results = allofit.data
-        callback(null, results)
+        request({
+            method: 'GET',
+            url: 'https://kitsu.io/api/edge/genres?page%5Blimit%5D=10&page%5Boffset%5D=' + (offset.toString() ? offset : '0'),
+            headers: {
+                'Content-Type': 'application/vnd.api+json',
+                'Accept': 'application/vnd.api+json'
+            }
+        }, function(error, response, body) {
+            if (error) {
+                reject(Error(error))
+            } else {
+                var allofit = JSON.parse(body)
+                var results = allofit.data
+                resolve(results)
+            }
+        })
     })
 }
 
-exports.findCharacter = function(name, offset, callback) {
-    if (offset === null || offset === undefined) {
-        offset = 0
-    }
-    request({
-        method: 'GET',
-        url: 'https://kitsu.io/api/edge/characters?filter[name]=' + name + '&page%5Blimit%5D=10&page%5Boffset%5D=' + (offset.toString() ? offset : '0'),
-        headers: {
-            'Content-Type': 'application/vnd.api+json',
-            'Accept': 'application/vnd.api+json'
+exports.findCharacter = function(name, offset) {
+    return new Promise(function(resolve, reject) {
+        if (offset === null || offset === undefined) {
+            offset = 0
         }
-    }, function(error, response, body) {
-        if (error) {
-            callback(error)
-        }
-        var allofit = JSON.parse(body)
-        var results = allofit.data
-        callback(null, results)
+        request({
+            method: 'GET',
+            url: 'https://kitsu.io/api/edge/characters?filter[name]=' + name + '&page%5Blimit%5D=10&page%5Boffset%5D=' + (offset.toString() ? offset : '0'),
+            headers: {
+                'Content-Type': 'application/vnd.api+json',
+                'Accept': 'application/vnd.api+json'
+            }
+        }, function(error, response, body) {
+            if (error) {
+                reject(Error(error))
+            } else {
+                var allofit = JSON.parse(body)
+                var results = allofit.data
+                resolve(results)
+            }
+        })
     })
 }
